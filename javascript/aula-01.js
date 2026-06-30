4d94fc883371bb68ba4c3a1cd45e005c5656f18a
Leandro lopes <leandrolopesdocarmo41@gmail.com>
2026-06-30 09:13:03 -0300

    exercício

--- javascript/aula-01.js
+++ javascript/aula-01.js
@@ -1,3 +1,64 @@
-alert("Olá,Mundo!");
-/*window.confirm("Gostou??");
- window.prompt("Qual é o seu nome");*/
+       //  Exercicio-001
+
+/*alert("Olá,Mundo!");
+window.confirm("Gostou??");
+window.prompt("Qual é o seu nome");*/
+
+      //  Exercicio-002
+
+/*var name = window.prompt("What's yor name"); // Pergunta seu nome.
+alert('Nice to meet you! ' + name)*/
+
+       // Atividade 02 
+//Para conversão de String para número usamos os comandos abixos
+//  obs: usar Number.parseInt(n) para convesão em numeros inteiros 
+// Number.parseFloat(n)para convesão de numeros reais ou com vigulas e ponto
+
+/*var n1 = Number.parseInt( window.prompt('Digite um número'))
+var n2 = Number.parseInt(window.prompt('Digite outro número'))
+var soma =n1+n2
+window.prompt('A soma dos valores é '+soma)
+
+// Concatenação
+// (number+number) para adição
+// (String+String) para cancatenação
+
+var n1 = Number.parseFloat( window.prompt('Digite um número'))
+var n2 = Number.parseFloat(window.prompt('Digite outro número'))
+var soma = n1+n2
+window.prompt('A soma dos valores é '+soma)
+
+var n1 = Number( window.prompt('Digite um número'))
+var n2 = Number(window.prompt('Digite outro número'))
+var soma = n1+n2
+window.prompt('A soma dos valores é '+soma)
+
+//Para conversão de Número para String usamos os comandos abixos
+//Usamos String(n), n.toString()
+
+       // Atividade 03
+// Usar Template string para interpolar placeholder
+var n1 = Number( window.prompt('Digite um número'))
+var n2 = Number(window.prompt('Digite outro número'))
+var soma = n1+n2
+window.prompt(`A soma dos valores ${n1} e ${n2} é igual a ${soma} `)
+
+// Var s = 'javaScript'
+// s.length para saber quantos caracteres a string tem
+// s.toUppercase() transfoma tudo em letras maiúsculas
+// stolowercase() transforma tudo em letras minúsculas
+
+// Exercicio 03
+
+var nome = window.prompt('Qual é seu nome')
+document.write(`Seu nome tem ${nome.length} letras.`)*/
+
+// Atividade 01
+
+/*var moeda = 1565.80
+moeda.toFixed(2)
+moeda.toFixed(2).replace('.',',')
+moeda.toLocaleString('pt-br', {style:'currency', currency:'BRL'})-->usado para moedas */
+
+// Exercício 04
+
